796bcb5dba5c61abf07ef5fa4c6c435799b814c7
Abhishek Bhattacharjee <ahishek1250@gmail.com>
2025-05-17 15:45:58 +0530

    Minor fix

--- app/error.jsx
+++ app/error.jsx
@@ -1,17 +1,18 @@
 "use client";
 
-export default function Error({ error, reset }) {
+import Link from "next/link";
+
+export default function Error() {
   return (
     <main className="flex justify-center items-center flex-col gap-6">
       <h1 className="text-3xl font-semibold">Something went wrong!</h1>
-      <p className="text-lg">{error.message}</p>
 
-      <button
-        onClick={reset}
+      <Link
+        href="/"
         className="inline-block bg-accent-500 text-primary-800 px-6 py-3 text-lg"
       >
         Try again
-      </button>
+      </Link>
     </main>
   );
 }
